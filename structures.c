89609cbb4907001f11e0283069e235570cdcebad
Itra7 <37014351+Itra7@users.noreply.github.com>
2019-04-10 18:58:25 +0200

    Update structures.c

--- structures.c
+++ structures.c
@@ -6,12 +6,12 @@ int store(){
 	char str[5000];
 	scanf("%[^'\n']s",str);
 	FILE *fp = fopen("myfile.txt", "a+");
-	 time_t current_time;
-    char* c_time_string;
+	time_t current_time;
+    	char* c_time_string;
 
-    /*current time. */
-    current_time = time(NULL);
-    c_time_string = ctime(&current_time);
+      		/*current time. */
+    	current_time = time(NULL);
+    	c_time_string = ctime(&current_time);
 
 	fprintf(fp, "\n %s  \n %s \n", c_time_string, str);
 	fclose(fp);	
